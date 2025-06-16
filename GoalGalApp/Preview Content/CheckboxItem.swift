a942f77b778999396939ce3b6e855e7a3c096acc
Claire Lister <claire.lister@flutteruki.com>
2025-06-16 18:46:02 +0100

    Adding checklist feature to DetailView

---
+++ GoalGalApp/Preview Content/CheckboxItem.swift
@@ -0,0 +1,12 @@
+//
+//  CheckboxItem.swift
+//  GoalGalApp
+//
+//  Created by Claire Lister on 16/06/2025.
+//
+import SwiftUI
+
+struct CheckboxItem {
+    var name: String
+    var isChecked: Bool
+}
